3ae3fff1b6f21ae81ccabd2d722c993d2ee6b4cc
Guan-Xin <alex0972112871@gmail.com>
2023-09-28 07:45:15 +0800

    Comment and close endpoints, only waiter endpoints are available.

--- apps/nest-datalayer-checker/src/inspector/inspector.controller.ts
+++ apps/nest-datalayer-checker/src/inspector/inspector.controller.ts
@@ -5,31 +5,31 @@ import { InspectorService } from './inspector.service';
 export class InspectorController {
   constructor(private readonly inspectorService: InspectorService) {}
 
-  @Get('/inspect/:projectName/:testName')
-  async inspectDataLayer(
-    @Param('projectName') projectName: string,
-    @Param('testName') testName: string,
-    @Query('headless') headless = 'false',
-    @Query('path') path?: string
-  ) {
-    return await this.inspectorService.inspectDataLayer(
-      projectName,
-      testName,
-      headless,
-      path
-    );
-  }
+  // @Get('/inspect/:projectName/:testName')
+  // async inspectDataLayer(
+  //   @Param('projectName') projectName: string,
+  //   @Param('testName') testName: string,
+  //   @Query('headless') headless = 'false',
+  //   @Query('path') path?: string
+  // ) {
+  //   return await this.inspectorService.inspectDataLayer(
+  //     projectName,
+  //     testName,
+  //     headless,
+  //     path
+  //   );
+  // }
 
-  @Get('/inspect/:projectName')
-  async inspectProjectDataLayer(
-    @Param('projectName') projectName: string,
-    @Query('headless') headless = 'true',
-    @Query('path') path?: string
-  ) {
-    return await this.inspectorService.inspectProjectDataLayer(
-      projectName,
-      headless,
-      path
-    );
-  }
+  // @Get('/inspect/:projectName')
+  // async inspectProjectDataLayer(
+  //   @Param('projectName') projectName: string,
+  //   @Query('headless') headless = 'true',
+  //   @Query('path') path?: string
+  // ) {
+  //   return await this.inspectorService.inspectProjectDataLayer(
+  //     projectName,
+  //     headless,
+  //     path
+  //   );
+  // }
 }
--- apps/nest-datalayer-checker/src/web-agent/web-agent.controller.ts
+++ apps/nest-datalayer-checker/src/web-agent/web-agent.controller.ts
@@ -5,72 +5,72 @@ import { WebAgentService } from './web-agent.service';
 export class WebAgentController {
   constructor(private readonly webAgentService: WebAgentService) {}
   // for demo purposes
-  @Get('/action/:projectName/:name')
-  async executeAndGetDataLayer(
-    @Param('projectName') projectName: string,
-    @Param('name') name: string,
-    @Query('args') args = [''],
-    @Query('headless') headless = 'false',
-    @Query('path') path?: string,
-    @Query('username') username?: string,
-    @Query('password') password?: string
-  ) {
-    return await this.webAgentService.executeAndGetDataLayer(
-      projectName,
-      name,
-      args,
-      headless,
-      path,
-      {
-        username,
-        password,
-      }
-    );
-  }
+  // @Get('/action/:projectName/:name')
+  // async executeAndGetDataLayer(
+  //   @Param('projectName') projectName: string,
+  //   @Param('name') name: string,
+  //   @Query('args') args = [''],
+  //   @Query('headless') headless = 'false',
+  //   @Query('path') path?: string,
+  //   @Query('username') username?: string,
+  //   @Query('password') password?: string
+  // ) {
+  //   return await this.webAgentService.executeAndGetDataLayer(
+  //     projectName,
+  //     name,
+  //     args,
+  //     headless,
+  //     path,
+  //     {
+  //       username,
+  //       password,
+  //     }
+  //   );
+  // }
 
-  @Get('/action/gtmValidation/:projectName/:name')
-  async executeAndGetDataLayerAndRequest(
-    @Param('projectName') projectName: string,
-    @Param('name') name: string,
-    @Query('args') args = [''],
-    @Query('headless') headless = 'false',
-    @Query('path') path?: string,
-    @Query('measurementId') measurementId?: string,
-    @Query('username') username?: string,
-    @Query('password') password?: string
-  ) {
-    return await this.webAgentService.executeAndGetDataLayerAndRequest(
-      projectName,
-      name,
-      args,
-      headless,
-      path,
-      measurementId,
-      {
-        username,
-        password,
-      }
-    );
-  }
+  // @Get('/action/gtmValidation/:projectName/:name')
+  // async executeAndGetDataLayerAndRequest(
+  //   @Param('projectName') projectName: string,
+  //   @Param('name') name: string,
+  //   @Query('args') args = [''],
+  //   @Query('headless') headless = 'false',
+  //   @Query('path') path?: string,
+  //   @Query('measurementId') measurementId?: string,
+  //   @Query('username') username?: string,
+  //   @Query('password') password?: string
+  // ) {
+  //   return await this.webAgentService.executeAndGetDataLayerAndRequest(
+  //     projectName,
+  //     name,
+  //     args,
+  //     headless,
+  //     path,
+  //     measurementId,
+  //     {
+  //       username,
+  //       password,
+  //     }
+  //   );
+  // }
 
-  @Get('/projects/:projectName')
-  async executeAndGetDataLayerByProject(
-    @Param('projectName') projectName: string,
-    @Query('args') args = [''],
-    @Query('headless') headless = 'false',
-    @Query('path') path?: string,
-    @Query('username') username?: string,
-    @Query('password') password?: string
-  ) {
-    return await this.webAgentService.executeAndGetDataLayerByProject(
-      projectName,
-      args,
-      headless,
-      path,
-      {
-        username,
-        password,
-      }
-    );
-  }
+  // @Get('/projects/:projectName')
+  // async executeAndGetDataLayerByProject(
+  //   @Param('projectName') projectName: string,
+  //   @Query('args') args = [''],
+  //   @Query('headless') headless = 'false',
+  //   @Query('path') path?: string,
+  //   @Query('username') username?: string,
+  //   @Query('password') password?: string
+  // ) {
+  //   return await this.webAgentService.executeAndGetDataLayerByProject(
+  //     projectName,
+  //     args,
+  //     headless,
+  //     path,
+  //     {
+  //       username,
+  //       password,
+  //     }
+  //   );
+  // }
 }
