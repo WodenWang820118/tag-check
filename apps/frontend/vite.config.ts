182f8f5174e7b3ff69a452db1f890dd362a69389
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-01-25 19:09:49 +0800

    feat: electron production build setup.

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  base: `./`,
   root: './',
   cacheDir: '../../node_modules/.vite/frontend',
 
