c00ea67168f0889c064341b65a7a2d9283763c84
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-02-16 10:21:03 +0800

    feat: specs endpoints 1. Enable and align json-server's backend service. 2. Add TODOs in the waiter-project service for further optimizations.

--- apps/nest-datalayer-checker/src/waiter/spec/waiter-spec.controller.ts
+++ apps/nest-datalayer-checker/src/waiter/spec/waiter-spec.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { WaiterSpecService } from './waiter-spec.service';
 
 @Controller('specs')
 export class WaiterSpecController {
   constructor(private waiterSpecService: WaiterSpecService) {}
+
+  @Get(':projectSlug')
+  async getSpecs(@Param('projectSlug') projectSlug: string) {
+    return await this.waiterSpecService.getSpecs(projectSlug);
+  }
+
+  @Get(':projectSlug/:eventName')
+  async getSpec(
+    @Param('projectSlug') projectSlug: string,
+    @Param('eventName') eventName: string
+  ) {
+    return await this.waiterSpecService.getSpec(projectSlug, eventName);
+  }
 }
