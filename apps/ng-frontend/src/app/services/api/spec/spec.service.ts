dc76345814befbf371aefc117f08e89c99584f78
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-02-18 11:38:42 +0800

    feat: spec endpoint 1. Enable and align spec endpoint to json-server's mock backend. 2. Ammend frontend code to use the endpoint accordingly.

--- apps/ng-frontend/src/app/services/api/spec/spec.service.ts
+++ apps/ng-frontend/src/app/services/api/spec/spec.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Subject, map } from 'rxjs';
 import { Spec } from '../../../models/spec.interface';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -16,13 +17,13 @@ export class SpecService {
   constructor(private http: HttpClient) {}
 
   getSpecs() {
-    return this.http.get<Spec[]>(this.mockUrl);
+    return this.http.get<Spec[]>(environment.specApiUrl);
   }
 
   getSpec(projectSlug: string, eventName: string) {
-    return this.http.get<Spec>(`${this.mockUrl}/${projectSlug}`).pipe(
+    return this.http.get<Spec>(`${environment.specApiUrl}/${projectSlug}`).pipe(
       map((project) => {
-        return project.specs.find((spec) => spec.title === eventName);
+        return project.specs.find((spec) => spec.event === eventName);
       })
     );
   }
