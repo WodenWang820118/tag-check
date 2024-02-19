b08490e359cb5c93e19a6743136c079a26e5ef37
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-02-19 22:03:58 +0800

    feat: image endpoint 1. Enable backend to provide images. 2. Ammend Angular frontend to receive images.

--- apps/ng-frontend/src/app/views/detail-view/detail-view.component.ts
+++ apps/ng-frontend/src/app/views/detail-view/detail-view.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, Subscription, tap } from 'rxjs';
-import { Router } from '@angular/router';
+import { Observable, Subscription, combineLatest, of, tap } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ReportDetailsService } from '../../services/report-details/report-details.service';
 import { ReportDetailPanelsComponent } from '../../components/report-detail-panels/report-detail-panels.component';
 import { ReportDetails } from '../../models/report.interface';
+import { ImageService } from '../../services/api/image/image.service';
+import { BlobToUrlPipe } from '../../pipe/blob-to-url-pipe';
 
 @Component({
   selector: 'app-detail-view',
   standalone: true,
-  imports: [CommonModule, ReportDetailPanelsComponent],
+  imports: [CommonModule, ReportDetailPanelsComponent, BlobToUrlPipe],
   template: ` <div class="detail">
     <div class="detail__header">
       <h1>{{ (reportDetails$ | async)?.eventName }}</h1>
@@ -18,7 +20,11 @@ import { ReportDetails } from '../../models/report.interface';
     </div>
     <div class="detail__content">
       <div class="detail__image">
-        <p>Image Placeholder</p>
+        <img
+          *ngIf="image$ | async as imageBlob"
+          [src]="imageBlob | blobToUrl"
+          alt="Loaded image"
+        />
       </div>
       <div class="detail__panels">
         <app-report-datail-panels
@@ -59,28 +65,40 @@ import { ReportDetails } from '../../models/report.interface';
 })
 export class DetailViewComponent implements OnInit, OnDestroy {
   reportDetails$: Observable<ReportDetails | undefined>;
+  image$!: Observable<Blob> | Observable<null>;
   subscriptions: Subscription[] = [];
 
   constructor(
     public reportDetailsService: ReportDetailsService,
-    private router: Router
+    private imageService: ImageService,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.reportDetails$ = this.reportDetailsService.reportDetails$;
   }
 
   ngOnInit(): void {
-    // TODO: how to do image retrieval from the json server?
-    const reportDetailsSubscription = this.reportDetails$
+    console.log('DetailViewComponent ngOnInit');
+    const reportDetailsSubscription = this.reportDetails$.pipe().subscribe();
+
+    const imageSubscription = combineLatest([
+      this.route.params,
+      this.route.parent?.params || of({ projectSlug: '' }),
+    ])
       .pipe(
-        tap((reportDetails) => {
-          if (!reportDetails) {
-            // this.router.navigate(['..']);
+        tap(([params, parentParams]) => {
+          if (params && parentParams) {
+            this.image$ = this.imageService.getImage(
+              parentParams['projectSlug'],
+              params['eventName']
+            );
           }
         })
       )
       .subscribe();
 
     this.subscriptions.push(reportDetailsSubscription);
+    this.subscriptions.push(imageSubscription);
   }
 
   ngOnDestroy() {
