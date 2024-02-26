372e08e0b26fbc468a3bd28836fe5128cb074679
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-02-27 05:56:01 +0800

    fix: add the try catch block to handle potential error.

--- apps/nest-backend/src/waiter/report/waiter-report.service.ts
+++ apps/nest-backend/src/waiter/report/waiter-report.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
 import { FileService } from '../../os/file/file.service';
 import { FolderService } from '../../os/folder/folder.service';
 import { FolderPathService } from '../../os/path/folder-path/folder-path.service';
@@ -19,36 +19,41 @@ export class WaiterReportService {
 
   // verified
   async getProjectReports(projectSlug: string) {
-    const folderNames = this.folderService.readFolderFileNames(
-      await this.folderPathService.getInspectionResultFolderPath(projectSlug)
-    );
-
-    const reportsPromise = folderNames.map(async (folderName) => {
-      const filePath = await this.filePathService.getInspectionResultFilePath(
-        projectSlug,
-        folderName,
-        ABSTRACT_REPORT_FILE_NAME
+    try {
+      const folderNames = this.folderService.readFolderFileNames(
+        await this.folderPathService.getInspectionResultFolderPath(projectSlug)
       );
 
-      const completedTime = statSync(filePath).mtime;
-      return {
-        eventName: folderName,
-        ...this.fileService.readJsonFile(
-          await this.filePathService.getInspectionResultFilePath(
-            projectSlug,
-            folderName,
-            ABSTRACT_REPORT_FILE_NAME
-          )
-        ),
-        completedTime,
-      };
-    });
+      const reportsPromise = folderNames.map(async (folderName) => {
+        const filePath = await this.filePathService.getInspectionResultFilePath(
+          projectSlug,
+          folderName,
+          ABSTRACT_REPORT_FILE_NAME
+        );
 
-    const reports = await Promise.all(reportsPromise);
-    return {
-      projectSlug: projectSlug,
-      reports: reports,
-    };
+        const completedTime = statSync(filePath).mtime;
+        return {
+          eventName: folderName,
+          ...this.fileService.readJsonFile(
+            await this.filePathService.getInspectionResultFilePath(
+              projectSlug,
+              folderName,
+              ABSTRACT_REPORT_FILE_NAME
+            )
+          ),
+          completedTime,
+        };
+      });
+
+      const reports = await Promise.all(reportsPromise);
+      return {
+        projectSlug: projectSlug,
+        reports: reports,
+      };
+    } catch (error) {
+      Logger.error(error);
+      throw new HttpException('Failed to get project reports', 500);
+    }
   }
 
   // TODO: haven't been tested
