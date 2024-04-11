c88256c6370dd15e93ecc7706927d68b610e37b0
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-04-11 21:02:41 +0800

    feat: expected value 1. Add the dollar sign '$' for checking keys only. 2. Add the regex literal for expected value. 3. Add the static value to be exactly matched.

--- apps/nest-backend/src/inspector/strategy/dataLayer-validation-strategy.spec.ts
+++ apps/nest-backend/src/inspector/strategy/dataLayer-validation-strategy.spec.ts
@@ -1,174 +1,166 @@
 import {
   BaseDataLayerEvent,
   StrictDataLayerEvent,
-} from '../../interfaces/dataLayer.interface';
+  ValidationResult,
+  ValidationStrategy,
+} from '@utils';
 import {
   EcommerceEventValidationStrategy,
   OldGA4EventsValidationStrategy,
-  ValidationStrategy,
-} from './dataLayer-validation-strategy'; // <- Change this to the actual import location
+} from './dataLayer-validation-strategy';
 
-describe('EcommerceEventValidationStrategy', () => {
-  let strategy: ValidationStrategy;
+describe('ValidationStrategy', () => {
+  describe('EcommerceEventValidationStrategy', () => {
+    let strategy: ValidationStrategy;
 
-  beforeEach(() => {
-    strategy = new EcommerceEventValidationStrategy();
-  });
+    beforeEach(() => {
+      strategy = new EcommerceEventValidationStrategy();
+    });
 
-  const tests: {
-    description: string;
-    dataLayer: BaseDataLayerEvent[] | StrictDataLayerEvent[];
-    spec: StrictDataLayerEvent;
-    expectedResult: {
-      passed: boolean;
-      message: string;
-    };
-  }[] = [
-    {
-      description: 'should pass for add_to_cart event with all required keys',
-      dataLayer: [
-        { ecommerce: null },
+    describe('validateDataLayer', () => {
+      const actualDataLayerObj = {
+        event: 'add_to_cart',
+        ecommerce: {
+          items: [
+            {
+              item_id: 'SKU_12345',
+              item_name: 'Switzerland',
+              item_category: 'Europe',
+              quantity: 1,
+              price: 200,
+            },
+          ],
+          currency: 'USD',
+          value: 200,
+        },
+      };
+
+      // the spec for the dataLayer object
+      // it has variables that only need to match the key
+      // it has regex for the values matching
+      // it has static string or number values which are exactly matching
+      const dataLayerSpec = {
+        event: 'add_to_cart',
+        ecommerce: {
+          items: [
+            {
+              item_id: '$item_id',
+              item_name: '^(?:switzerland|california)$',
+              item_category: 'Europe',
+              quantity: 1,
+              price: 200,
+            },
+          ],
+          currency: 'USD',
+          value: 200,
+        },
+      };
+
+      const tests: {
+        description: string;
+        dataLayer: BaseDataLayerEvent[] | StrictDataLayerEvent[];
+        dataLayerSpec: StrictDataLayerEvent;
+        expectedResult: ValidationResult;
+      }[] = [
         {
+          description:
+            'should pass for add_to_cart event with all required keys/values',
+          dataLayer: [{ ecommerce: null }, actualDataLayerObj],
+          dataLayerSpec: dataLayerSpec,
+          expectedResult: {
+            passed: true,
+            message: 'Valid',
+            dataLayerSpec: dataLayerSpec,
+          },
+        },
+      ];
+
+      it.each(tests)(
+        '$description',
+        ({ dataLayer, dataLayerSpec, expectedResult }) => {
+          const result = strategy.validateDataLayer(dataLayer, dataLayerSpec);
+          expect(result.passed).toBe(expectedResult.passed);
+          expect(result.message).toBe(expectedResult.message);
+        }
+      );
+
+      it('should fail if ecommerce is not reset', () => {
+        const dataLayer: BaseDataLayerEvent[] | StrictDataLayerEvent[] = [
+          {
+            event: 'view_item',
+            ecommerce: {
+              items: [
+                {
+                  item_id: 'SKU_12345',
+                  item_name: 'Laptop',
+                },
+              ],
+            },
+          },
+          {
+            event: 'add_to_cart',
+            ecommerce: {
+              items: [
+                {
+                  item_id: 'SKU_12346',
+                  item_name: 'Mouse',
+                },
+              ],
+            },
+          },
+        ];
+        const spec: StrictDataLayerEvent = {
           event: 'add_to_cart',
           ecommerce: {
             items: [
               {
-                item_id: 'SKU_12345',
-                item_name: 'Laptop',
+                item_id: 'SKU_12346',
+                item_name: 'Mouse',
               },
             ],
           },
-        },
-      ],
-      spec: {
-        event: 'add_to_cart',
-        ecommerce: {
-          items: [
-            {
-              item_id: 'SKU_12345',
-              item_name: 'Laptop',
-            },
-          ],
-        },
-      },
-      expectedResult: {
-        passed: true,
-        message: 'Valid',
-      },
-    },
-    {
-      description: 'should pass for view_item event with all required keys',
-      dataLayer: [
-        { ecommerce: null },
-        {
-          event: 'view_item',
-          ecommerce: {
-            items: [
-              {
-                item_id: 'SKU_12345',
-                item_name: 'Laptop',
-              },
-            ],
-          },
-        },
-      ],
-      spec: {
-        event: 'view_item',
-        ecommerce: {
-          items: [
-            {
-              item_id: 'SKU_12345',
-              item_name: 'Laptop',
-            },
-          ],
-        },
-      },
-      expectedResult: {
-        passed: true,
-        message: 'Valid',
-      },
-    },
-    // ... more test cases
-  ];
+        };
+        const result = strategy.validateDataLayer(dataLayer, spec);
 
-  it.each(tests)('$description', ({ dataLayer, spec, expectedResult }) => {
-    const result = strategy.validateDataLayer(dataLayer, spec);
-    expect(result.passed).toBe(expectedResult.passed);
-    expect(result.message).toBe(expectedResult.message);
+        expect(result.passed).toBe(false);
+        expect(result.message).toBe(
+          `ecommerce must be reset before firing ${spec.event}.`
+        );
+      });
+    });
   });
 
-  it('should fail if ecommerce is not reset', () => {
-    const dataLayer: BaseDataLayerEvent[] | StrictDataLayerEvent[] = [
-      {
-        event: 'view_item',
-        ecommerce: {
-          items: [
-            {
-              item_id: 'SKU_12345',
-              item_name: 'Laptop',
-            },
-          ],
-        },
-      },
-      {
-        event: 'add_to_cart',
-        ecommerce: {
-          items: [
-            {
-              item_id: 'SKU_12346',
-              item_name: 'Mouse',
-            },
-          ],
-        },
-      },
-    ];
-    const spec: StrictDataLayerEvent = {
-      event: 'add_to_cart',
-      ecommerce: {
-        items: [
-          {
-            item_id: 'SKU_12346',
-            item_name: 'Mouse',
-          },
-        ],
-      },
-    };
-    const result = strategy.validateDataLayer(dataLayer, spec);
+  describe('OldGA4EventsValidationStrategy', () => {
+    let strategy: ValidationStrategy;
 
-    expect(result.passed).toBe(false);
-    expect(result.message).toBe('ecommerce not reset');
-  });
-});
+    beforeEach(() => {
+      strategy = new OldGA4EventsValidationStrategy();
+    });
 
-describe('OldGA4EventsValidationStrategy', () => {
-  let strategy: ValidationStrategy;
+    it('should pass for generate_lead event with all keys', () => {
+      const dataLayer: StrictDataLayerEvent[] = [
+        { event: 'generate_lead', value: 10, currency: 'USD' },
+      ];
+      const spec: StrictDataLayerEvent = {
+        event: 'generate_lead',
+        value: 10,
+        currency: 'USD',
+      };
+      const result = strategy.validateDataLayer(dataLayer, spec);
 
-  beforeEach(() => {
-    strategy = new OldGA4EventsValidationStrategy();
-  });
+      expect(result.passed).toBe(true);
+      expect(result.message).toBe('Valid');
+    });
 
-  it('should pass for generate_lead event with all keys', () => {
-    const dataLayer: StrictDataLayerEvent[] = [
-      { event: 'generate_lead', value: 10, currency: 'USD' },
-    ];
-    const spec: StrictDataLayerEvent = {
-      event: 'generate_lead',
-      value: 10,
-      currency: 'USD',
-    };
-    const result = strategy.validateDataLayer(dataLayer, spec);
+    it('should fail for login event without method key', () => {
+      const dataLayer: StrictDataLayerEvent[] = [{ event: 'login' }];
+      const spec: StrictDataLayerEvent = { event: 'login', method: 'Google' };
+      const result = strategy.validateDataLayer(dataLayer, spec);
 
-    expect(result.passed).toBe(true);
-    expect(result.message).toBe('Valid');
-  });
-
-  it('should fail for login event without method key', () => {
-    const dataLayer: StrictDataLayerEvent[] = [{ event: 'login' }];
-    const spec: StrictDataLayerEvent = { event: 'login', method: 'Google' };
-    const result = strategy.validateDataLayer(dataLayer, spec);
-
-    expect(result.passed).toBe(false);
-    expect(result.message).toBe('Missing keys');
-    expect(result.incorrectInfo).toEqual(['method']);
+      expect(result.passed).toBe(false);
+      expect(result.message).toBe(
+        `Key "method" is not present in the dataLayer`
+      );
+    });
   });
 });
