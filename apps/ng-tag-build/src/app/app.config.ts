6281f97b23076761e610c15015df704299c9b61b
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-04-15 18:36:51 +0800

    refactor: tag build extracted as an library 1. Move components, services and other related interfaces to the Angular library. 2. Adjust component naming. 3. Refactor Tag Build and address SEO.

--- apps/ng-tag-build/src/app/app.config.ts
+++ apps/ng-tag-build/src/app/app.config.ts
@@ -1,12 +1,10 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(), // required
     provideHttpClient(), // required
-    importProvidersFrom(SharedModule),
   ],
 };
