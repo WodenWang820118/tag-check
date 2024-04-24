aa597c48961e9892ab06e9ba29222db00b4e4357
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-04-24 08:53:47 +0800

    feat: complete help center layout

--- apps/ng-frontend/src/app/modules/help-center/components/side-bar/side-bar.component.ts
+++ apps/ng-frontend/src/app/modules/help-center/components/side-bar/side-bar.component.ts
@@ -1,18 +1,46 @@
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
+import { MatTreeNestedDataSource, MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { TopicNode, TREE_DATA } from '../../tree-data';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>side bar works</p>`,
-  styles: [``],
+  imports: [
+    CommonModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+  ],
+  templateUrl: 'side-bar.component.html',
+  styleUrls: ['side-bar.component.scss'],
 })
 export class SideBarComponent implements OnDestroy {
   destroy$ = new Subject<void>();
+  treeControl = new NestedTreeControl<TopicNode>((node) => node.children);
+  dataSource = new MatTreeNestedDataSource<TopicNode>();
+  selectedNodeContent: string = '';
 
-  constructor() {}
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: TopicNode) =>
+    !!node.children && node.children.length > 0;
+
+  onNodeClick(nodeName: string): void {
+    // Your logic to show contents based on node name
+    // For demonstration, I'll simply set the selectedNodeContent
+    this.selectedNodeContent = `Contents related to ${nodeName}`;
+    console.log(this.selectedNodeContent);
+    console.log(nodeName);
+  }
 
   ngOnDestroy() {
     this.destroy$.next();
--- apps/ng-frontend/src/app/modules/help-center/views/help-center-view.component.ts
+++ apps/ng-frontend/src/app/modules/help-center/views/help-center-view.component.ts
@@ -1,18 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
-import { ConentsComponent } from '../components/contents/contents.component';
+import { MainContentComponent } from '../components/main-content/main-content.component';
 import { SideBarComponent } from '../components/side-bar/side-bar.component';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-help-center-view',
   standalone: true,
-  imports: [CommonModule, ConentsComponent, SideBarComponent],
+  imports: [CommonModule, MainContentComponent, SideBarComponent, RouterModule],
   template: `
-    <app-side-bar></app-side-bar>
-    <app-contents></app-contents>
+    <div class="help-center">
+      <app-side-bar class="sidebar"></app-side-bar>
+      <div class="main-content">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
   `,
-  styles: [``],
+  styles: [
+    `
+      .help-center {
+        display: grid;
+        grid-template-columns: 250px 1fr;
+        grid-gap: 20px;
+        height: 100%;
+      }
+
+      .sidebar {
+        background-color: #f0f0f0;
+        padding: 20px;
+      }
+
+      .main-content {
+        background-color: #fff;
+        padding: 20px;
+      }
+    `,
+  ],
 })
 export class HelpCenterComponent implements OnDestroy {
   destroy$ = new Subject<void>();
