892f50e743fe22a69a44781342138c339484e09d
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-04-30 05:38:19 +0800

    refactor: toolbar 1. Change the text to icon buttons for UX. 2. Add a global setting page menu and its page.

--- apps/ng-frontend/src/app/modules/entry/entry.module.ts
+++ apps/ng-frontend/src/app/modules/entry/entry.module.ts
@@ -4,11 +4,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { EntryComponent } from './entry.component';
 import { HomeViewComponent } from './views/home-view/home-view.component';
 import { InitProjectViewComponent } from './views/init-project-view/init-project-view.component';
+import { GlobalSettingsViewComponent } from './views/global-settings-view/global-settings-view.component';
 
 const ENTRY_ROUTES: Routes = [
   {
     path: 'init-project',
     component: InitProjectViewComponent,
+  },
+  {
+    path: 'global-settings',
+    component: GlobalSettingsViewComponent,
   },
   {
     path: '',
