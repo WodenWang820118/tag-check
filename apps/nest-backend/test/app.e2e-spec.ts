6c1c0018bb8fa1332a7f16481f94302e50e2a5d4
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-09-18 15:23:46 +0800

    feat: add sonar for code quality examinations

--- apps/nest-backend/test/app.e2e-spec.ts
+++ apps/nest-backend/test/app.e2e-spec.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
