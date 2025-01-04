b33d6bd6ba19da06e2c891c94d1183295513ed21
Guan-Xin Wang <guanxinwang0118@gmail.com>
2025-01-04 17:05:58 +0800

    feat: i18n about, objectives page, and svg

--- libs/ui/src/lib/components/about/about.component.ts
+++ libs/ui/src/lib/components/about/about.component.ts
@@ -1,19 +1,51 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'lib-about',
   standalone: true,
-  template: `
-    <h1>About</h1>
-    <p>
-      The GTM Tag Build is a user-friendly tool designed for digital marketers
-      and SEO experts to generate a JSON file for Google Tag Manager. This tool
-      streamlines the tag management process by focusing on data analysis and
-      reducing technical complexities. With the GTM Tag Build, users can
-      effortlessly create accurate and optimized JSON files, enhancing
-      productivity and ensuring error-free tag management.
-    </p>
-  `,
-  styles: []
+  imports: [JsonPipe],
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {}
+export class AboutComponent {
+  exampleInput = {
+    comments: '----- usual event data -----',
+    event: 'begin_checkout',
+    ecommerce: {
+      value: '$value',
+      currency: '$currency',
+      items: [
+        {
+          item_id: '$item_id',
+          item_name: '$item_name',
+          item_brand: '$item_brand',
+          item_category: '$item_category',
+          item_category2: '$item_category2',
+          item_category3: '$item_category3',
+          item_category4: '$item_category4',
+          item_category5: '$item_category5',
+          discount: '$discount',
+          price: '$value',
+          quantity: '$quantity',
+          coupon: '$coupon',
+          index: '$index',
+          item_variant: '$item_variant'
+        }
+      ]
+    }
+  };
+  exampleArrayInput = [
+    {
+      comments: '----- Tag Build format -----',
+      event: 'another_event'
+    },
+    this.exampleInput
+  ];
+
+  constructor(@Inject(LOCALE_ID) private locale: string) {}
+
+  getLocalizedSvgPath(): string {
+    return `assets/i18n/${this.locale}/tag_build_system_${this.locale}.drawio.svg`;
+  }
+}
--- libs/ui/src/lib/components/objectives/objectives.component.ts
+++ libs/ui/src/lib/components/objectives/objectives.component.ts
@@ -1,20 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
   selector: 'lib-objectives',
   standalone: true,
-  template: `
-    <h1>Objectives</h1>
-    <p>
-      The main objective of the GTM Tag Build is to provide a user-friendly
-      interface for digital marketers and SEO experts to generate a JSON file
-      for Google Tag Manager. This tool aims to streamline the tag management
-      process by focusing on data analysis and reducing technical complexities.
-      With the GTM Tag Build, users can effortlessly create accurate and
-      optimized JSON files, enhancing productivity and ensuring error-free tag
-      management.
-    </p>
-  `,
-  styles: []
+  templateUrl: './objectives.component.html',
+  styleUrls: ['./objectives.component.scss']
 })
-export class ObjectivesComponent {}
+export class ObjectivesComponent {
+  constructor(@Inject(LOCALE_ID) private locale: string) {}
+  getLocalizedSvgPath(): string {
+    return `assets/i18n/${this.locale}/tag_check_system_${this.locale}.drawio.svg`;
+  }
+}
