d0ea3b9f82bc05427c3c71c3dac516a824e7a4c0
Guan-Xin Wang <guanxinwang0118@gmail.com>
2025-03-03 04:41:07 +0800

    fix: Playwright test for Electron app

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,13 +1,33 @@
 import { defineConfig } from 'vite';
 import { builtinModules } from 'module';
+import { resolve } from 'path';
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['sqlite3', ...builtinModules]
-    },
-    commonjsOptions: {
-      ignoreDynamicRequires: true
-    }
-  }
-});
+export default process.env.NODE_ENV === 'dev'
+  ? defineConfig({
+      build: {
+        outDir: '.', // Output to a dist folder at the root of your project
+        rollupOptions: {
+          external: ['sqlite3', ...builtinModules],
+          output: {
+            entryFileNames: 'main.js', // Output as main.js directly
+            format: 'cjs' // Use CommonJS format for Node.js compatibility
+          },
+          input: {
+            main: resolve(__dirname, 'src/main.ts') // Your main process entry file
+          }
+        },
+        commonjsOptions: {
+          ignoreDynamicRequires: true
+        }
+      }
+    })
+  : defineConfig({
+      build: {
+        rollupOptions: {
+          external: ['sqlite3', ...builtinModules]
+        },
+        commonjsOptions: {
+          ignoreDynamicRequires: true
+        }
+      }
+    });
