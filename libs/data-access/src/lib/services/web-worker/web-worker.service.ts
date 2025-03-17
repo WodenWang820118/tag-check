e897cbf59c124e11f33d556bd6661ddc4b6b181f
Guan-Xin Wang <guanxinwang0118@gmail.com>
2025-03-18 07:22:15 +0800

    fix: storybook build issue; delete unused web worker

--- libs/data-access/src/lib/services/web-worker/web-worker.service.ts
+++ libs/data-access/src/lib/services/web-worker/web-worker.service.ts
@@ -9,17 +9,16 @@ export class WebWorkerService {
   private worker: Worker | undefined;
 
   constructor() {
-    if (typeof Worker !== 'undefined') {
-      // the error occurs and cannot be resolved when initialing tests with import.meta.url
-      this.worker = new Worker(new URL('../../app.worker.ts', import.meta.url));
-      // this.worker = new Worker(new URL('../../app.worker.ts'));
-
-      this.worker.onmessage = ({ data }) => {
-        this.subject.next(data);
-      };
-    } else {
-      console.warn('Web Workers are not supported in this environment.');
-    }
+    // if (typeof Worker !== 'undefined') {
+    //   // the error occurs and cannot be resolved when initialing tests with import.meta.url
+    //   this.worker = new Worker(new URL('../../app.worker.ts', import.meta.url));
+    //   // this.worker = new Worker(new URL('../../app.worker.ts'));
+    //   this.worker.onmessage = ({ data }) => {
+    //     this.subject.next(data);
+    //   };
+    // } else {
+    //   console.warn('Web Workers are not supported in this environment.');
+    // }
   }
 
   private subject = new Subject<any>();
