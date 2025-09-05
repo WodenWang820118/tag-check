dc2bacb33c2fb705dbe34f0e4ac2b31d5dacdcb0
MSI\guanxin <guanxinwang0118@gmail.com>
2025-09-05 10:05:10 +0800

    hotfix: related path .js leads to prod issue

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,8 @@
 import { join } from 'path';
 import electronLog from 'electron-log';
-import * as environmentUtils from './environment-utils.js';
-import * as pathUtils from './path-utils.js';
-import log from 'electron-log/main.js';
+import * as environmentUtils from './environment-utils';
+import * as pathUtils from './path-utils';
+import log from 'electron-log/main';
 log.initialize();
 
 // For example, set a custom log file path based on your environment:
