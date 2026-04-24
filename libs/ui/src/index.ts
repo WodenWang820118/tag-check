eb6274fd5d8b93799e28f4c9f2c5b17fcbcbdd7f
MSI\guanxin <guanxinwang0118@gmail.com>
2026-04-24 10:04:55 +0800

    refactor: clarify product doc shared ui boundaries

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,16 +1,4 @@
-// tag build components
-export * from './lib/views/tag-build-page/tag-build-page.component';
-export * from './lib/components/about/about.component';
-export * from './lib/components/objectives/objectives.component';
-export * from './lib/components/toolbar/toolbar.component';
-export * from './lib/components/footer/footer.component';
-
-// shared components
-export * from './lib/components/error-dialog/error-dialog.component';
-export * from './lib/components/progress-spinner/progress-spinner.component';
-export * from './lib/components/custom-mat-table/custom-mat-table.component';
-export * from './lib/components/editor/editor.component';
-
-// modules
-export * from './lib/modules/tag-build-app/tag-build-app.component';
-export * from './lib/modules/documentation/routes';
+export * from './lib/public-api/documentation';
+export * from './lib/public-api/shared-chrome';
+export * from './lib/public-api/shared-components';
+export * from './lib/public-api/tag-build';
